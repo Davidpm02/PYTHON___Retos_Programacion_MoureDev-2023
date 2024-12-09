512e566ab219ac9458acc63376b29ab73de63904
Davidpm02 <davidpamu02@gmail.com>
2024-12-09 22:14:12 +0100

    Nuevo reto resuelto

--- Parametros URL [011]/challenge.py
+++ Parametros URL [011]/challenge.py
@@ -9,3 +9,33 @@ DESCRIPTION:
  * los parámetros serían ["2023", "0"]
  */
 """
+
+## IMPORTS -----
+from urllib.parse import urlparse
+from typing import List
+
+def get_url_params(url:str) -> List[str]:
+    
+    """
+    Parsea una URL dada y retorna una lista con todos los
+    parámetros que esta contenga.
+    
+    params:
+        url (str) -- URL a pasear.
+    returns:
+        List[str] -- Lista con todos los parámetros enviados 
+        a la URL. 
+        En caso de que la URL no contenga parámetro, se devolverá
+        una lista vacía.
+    """
+    
+    parse_result = urlparse(url)
+    
+    # Accedo a los parámetros enviados a la URL
+    url_query = parse_result[4]
+    components_url_query = url_query.split("&")
+    url_params = [param.split("=")[-1] for param in components_url_query]
+    
+    return url_params
+    
+    
