c0dec02d7fdd8c3fe2d7e91ccf8bec04df0d03cf
Davidpm02 <davidpamu02@gmail.com>
2024-12-09 12:09:43 +0100

    Nuevo reto resuelto

--- La API (APPLICATION PROGRAMMING INTERFACE) [010]/challenge.py
+++ La API (APPLICATION PROGRAMMING INTERFACE) [010]/challenge.py
@@ -12,3 +12,34 @@ DESCRIPTION:
  */
 """
 
+## IMPORTS -----
+import urllib.request
+
+
+## FUNCTIONS -----
+
+def get_superhero_namelist():
+    
+    """
+    Obtiene un listado con los nombres de superheroes que 
+    coinciden con el nombre proporcionado por el usuario,
+    e imprime el resultado por pantalla.
+    
+    params:
+    
+    returns:
+    
+    """
+    
+    ref_name = input('Por favor, escribe el nombre del superheroe:')
+    url_api = f"https://superheroapi.com/api/a6e554363e55bbc08b8e995ac29d5872/search/{ref_name}"
+
+    # La API retorna un objeto con la respuesta
+    api_response = dict(urllib.request.urlopen(url_api))
+    superhero_namelist = []
+    for result in api_response['results']:
+        superhero_namelist.append(result['name'])
+    
+    print('Los superheroes coincidentes con el nombre proporcionado son:', end="\n")
+    for superhero_name in superhero_namelist:
+        print("  -", superhero_name, end='\n')
