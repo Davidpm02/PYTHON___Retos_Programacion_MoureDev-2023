796459db3992eca772bb00cdd9745cc792ee71bd
Davidpm02 <davidpamu02@gmail.com>
2025-03-08 21:02:01 +0100

    Descripción nuevo reto

--- Web Scraping [018]/challenge.py
+++ Web Scraping [018]/challenge.py
@@ -14,3 +14,31 @@ DESCRIPTION:
  
 """
 
+import requests
+from bs4 import BeautifulSoup
+
+# URL del sitio web del "Hola Mundo Day"
+URL = "https://holamundo.day"
+
+# Ejecuto una petición GET
+response = requests.get(URL)
+
+# Parseando el contenido de la respuesta a la petición
+text_response = response.text
+content_response = BeautifulSoup(text_response, "html.parser")
+
+# Obtengo el contenido de todas las etiquetas "div" del HTML
+div_elements = content_response.find_all('div')
+
+# Itero sobre los elementos 'div' y me quedo con aquel que contiene
+# la información que me interesa
+for div_element in div_elements:
+    h2_tags = div_element.find_all('h2')  # Busco todos los <h2> dentro del <div>
+        
+    for h2 in h2_tags:
+        if h2.text.strip() == 'Agenda':  # Comparo el texto eliminando espacios en blanco
+            selected_div = div_element
+            break  # Salgo del bucle interno si encuentro el elemento
+
+print(div_element)
+# Obtengo el contenido de la agenda marcada para el "Hola Mundo Day"
